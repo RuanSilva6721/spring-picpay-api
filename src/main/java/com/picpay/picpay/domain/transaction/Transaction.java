ce131fee166c58d3b3f61c17c040167f332c041a
Ruan Felipe <ruansilva6721@gmail.com>
2024-02-20 20:46:12 -0300

    git init

---
+++ src/main/java/com/picpay/picpay/domain/transaction/Transaction.java
@@ -0,0 +1,31 @@
+package com.picpay.picpay.domain.transaction;
+
+import com.picpay.picpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transaction")
+@Table(name = "transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+
+    private User receiver;
+    private LocalDateTime timestamp;
+
+}
